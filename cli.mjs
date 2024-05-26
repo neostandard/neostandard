8cee0e4fc210d0c6befebc3475cb761c4fb6d07d
Pelle Wessman <pelle@kodfabrik.se>
2024-05-26 14:23:23 +0200

    WIP: Test against existing ecosystem (#8)
    * Initial ecosystem data
    
    * Update data
    
    * Update dependency + some lists
    
    * Update to limit dependents to their latest version
    
    * Ignore list-dependents-cli in knip
    
    * Add compatibility workflow
    
    * fix file path
    
    * Fix jq script
    
    * Fix again
    
    * Fix jq
    
    * Tweak
    
    * Fix cache
    
    * Tweak cache even more
    
    * More testing
    
    * Only report errors for now
    
    * Fix for quiet
    
    * Test against modules that have up to date versions
    
    * Fix regression in `ts.js`
    
    * Update knip config
    
    * Enforce unique repo names
    
    * Test if a `noTs` makes more modules pass
    
    * Add a CLI to generate configs
    
    * Add migration step to config cli
    
    * Migrate the config before the tests
    
    * Improved error handling in migration
    
    * Fix formatting of generated configs
    
    * Reference local module in config
    
    * Fix globals option
    
    * Add support for `standard.env`
    
    Fixes: #9
    
    * Enable introspection of generated config
    
    * Only check thos eon latest version of standard
    
    * Run on even more packages
    
    Max limit is 256 in a matrix: https://docs.github.com/en/actions/using-jobs/using-a-matrix-for-your-jobs#using-a-matrix-strategy
    
    * Limit comp tests while in private, to save actions

---
+++ cli.mjs
@@ -0,0 +1,167 @@
+#!/usr/bin/env node
+
+import { readFile } from 'node:fs/promises'
+import { join } from 'node:path'
+
+import { peowly } from 'peowly'
+
+import { isStringArray } from './lib/utils.js'
+
+const packagePath = new URL('./package.json', import.meta.url)
+// type-coverage:ignore-next-line
+const pkg = JSON.parse(await readFile(packagePath, { encoding: 'utf8' }))
+
+const {
+  flags: {
+    esm,
+    migrate,
+    ...flags
+  },
+} = peowly({
+  options: {
+    env: {
+      listGroup: 'Config options',
+      description: 'Legacy environment definition, eg. "mocha". --global is preferred',
+      type: 'string',
+      multiple: true,
+    },
+    esm: {
+      listGroup: 'Output options',
+      description: 'Outputs as ESM instead of as CJS',
+      type: 'boolean',
+    },
+    global: {
+      listGroup: 'Config options',
+      description: 'Adds the provided string as a global',
+      type: 'string',
+      multiple: true,
+      short: 'g',
+    },
+    ignore: {
+      listGroup: 'Config options',
+      description: 'Ignores the provided pattern when linting files',
+      type: 'string',
+      multiple: true,
+      short: 'i',
+    },
+    'no-style': {
+      listGroup: 'Config options',
+      description: 'Deactivates all style linting',
+      type: 'boolean',
+    },
+    'no-ts': {
+      listGroup: 'Config options',
+      description: 'Deactivates all TypeScript based linting',
+      type: 'boolean',
+    },
+    migrate: {
+      listGroup: 'Input options',
+      description: 'Migrates the standardjs config in the package.json in the current directory',
+      type: 'boolean',
+    },
+    semi: {
+      listGroup: 'Config options',
+      description: 'Enforces rather than bans semi-colons',
+      type: 'boolean',
+    },
+  },
+  description: 'Generate a neostandard config for eslint',
+  examples: [
+    '--semi --no-style > eslint.config.js',
+  ],
+  name: 'neostandard',
+  pkg,
+})
+
+const flagMapping = /** @satisfies {Record<keyof typeof flags, keyof import('./index.js').NeostandardOptions>} */ ({
+  env: 'env',
+  global: 'globals',
+  ignore: 'ignores',
+  'no-style': 'noStyle',
+  'no-ts': 'noTs',
+  semi: 'semi',
+})
+
+const flagKeys = /** @type {Array<keyof typeof flagMapping>} */ (Object.keys(flagMapping))
+
+/** @type {Partial<typeof flags> & { env?: string[] }} */
+const flagsFromMigration = {}
+
+/**
+ * @template {string} T
+ * @template {T extends `${infer U}s` ? U : T} R
+ * @param {T} value
+ * @returns {R}
+ */
+function ensureSingular (value) {
+  return /** @type {R} */ (value.endsWith('s') ? value.slice(0, -1) : value)
+}
+
+if (migrate) {
+  /** @type {unknown} */
+  let sourcePkg
+
+  try {
+    sourcePkg = JSON.parse(await readFile(join(process.cwd(), 'package.json'), 'utf8'))
+  } catch (cause) {
+    console.log('Failed to read package.json:', cause)
+    process.exit(1)
+  }
+
+  if (sourcePkg && typeof sourcePkg === 'object' && 'standard' in sourcePkg && sourcePkg.standard && typeof sourcePkg.standard === 'object') {
+    for (const [rawKey, value] of Object.entries(sourcePkg.standard)) {
+      const key = ensureSingular(rawKey)
+
+      if (key === 'global' || key === 'ignore' || key === 'env') {
+        if (typeof value === 'string') {
+          flagsFromMigration[key] = [value]
+        } else if (Array.isArray(value) && isStringArray(value)) {
+          flagsFromMigration[key] = value
+        } else {
+          console.log(`Invalid migration value for "standard.${key}". Expected an array of strings, got:`, value)
+          process.exit(1)
+        }
+      } else {
+        console.warn(`Migration for "standard.${key}" is not yet supported. Open an issue at https://github.com/neostandard/neostandard`)
+      }
+    }
+  }
+}
+
+/** @type {string[]} */
+const config = []
+
+for (const flag of flagKeys) {
+  const newValue = flags[flag]
+  const migratedValue = flagsFromMigration[flag]
+
+  const value = Array.isArray(newValue) && Array.isArray(migratedValue)
+    ? [...newValue, ...migratedValue]
+    : (newValue || migratedValue)
+
+  if (value) {
+    const formattedValue = JSON.stringify(value)
+      .replaceAll('"', '\'')
+      .replaceAll('\',\'', '\', \'')
+
+    config.push(`${flagMapping[flag]}: ${formattedValue}`)
+  }
+}
+
+const formattedConfig = config.length > 1
+  ? `{\n  ${config.join(',\n  ')},\n}`
+  : (
+      config.length
+        ? `{ ${config.join(', ')} }`
+        : '{}'
+    )
+const semiEnding = flags.semi ? ';' : ''
+
+console.log(
+  esm
+    ? `import neostandard from 'neostandard'${semiEnding}
+
+export default neostandard(${formattedConfig})${semiEnding}`
+    : `'use strict'${semiEnding}
+
+module.exports = require('neostandard')(${formattedConfig})${semiEnding}`)
---
+++ dependents-data/README.md
@@ -0,0 +1,35 @@
+# Dependents data
+
+## Update using
+
+```sh
+npm run dependents-update
+```
+
+Or, to update a single one:
+
+```sh
+npm run dependents-update-one -- standard
+```
+
+## Refresh using
+
+```sh
+npm run dependents-refresh
+```
+
+Or, to refresh a single one:
+
+```sh
+npm run dependents-refresh-one -- standard
+```
+
+## Filter using
+
+```sh
+npm run dependents-filter
+```
+
+## Add dependents for another project
+
+Add the module to [`projects.json`](./projects.json)
