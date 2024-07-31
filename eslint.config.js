34b2a21be8471fbb420ffb47a2346f59bb0bb492
Pelle Wessman <pelle@kodfabrik.se>
2024-07-31 15:08:42 +0200

    test: add tests for `globals` and `env`

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,8 @@
 'use strict'
 
 module.exports = require('./')({
+  globals: ['assertsFoo'],
+  env: ['mocha'],
   ignores: [
     ...require('./').resolveIgnoresFromGitignore(),
     '**/should-be-ignored/**',
