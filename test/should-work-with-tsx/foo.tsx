e8e75e143bd70fdc53eac421706dbea1770a6713
Pelle Wessman <pelle@kodfabrik.se>
2024-07-31 14:29:48 +0200

    feat: port jsx support from `standard`
    Fixes #11
    
    Co-authored-by: Radek Duchon <radek.duchon@hotmail.com>
    Co-authored-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

---
+++ test/should-work-with-tsx/foo.tsx
@@ -0,0 +1,13 @@
+function MainButton () {}
+
+module.exports = function TodoList () {
+  return (
+    <>
+      <MainButton
+        src='https://i.imgur.com/yXOvdOSs.jpg'
+        alt='Hedy Lamarr'
+        className='photo'
+      />
+    </>
+  )
+}
